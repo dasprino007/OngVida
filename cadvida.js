8847151252a4b0063d506294119b1db853039421
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-01 09:40:47 -0300

    projeto na parte de banco completo mas o from não esta funcionado

--- cadvida.js
+++ cadvida.js
@@ -1,56 +1,82 @@
-//requisitando modules
+//requisitando os modulos
 const mongoose = require("mongoose");
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 //configurando o express para o postman e para usar a pagina
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+const port = 3000;
 
 //configurando o banco de dados
-
-mongoose.connect("mongodb://127.0.0.1:27017/dbvida",{
-    useNewUrlParser: true,
-    useUnifiedTopology : true
+mongoose.connect("mongodb://127.0.0.1:27017/dbvida", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 //criando a model do seu projeto
 const UsuarioSchema = new mongoose.Schema({
-    nome : {type : String},
-    email : {type : String, required : true},
-    endereco : {type : String},
-    numero : {type : Number},
-    cep : {type : String, required : true},
-    nascimento : {type : Date, required : true}
-})
-
-const Usuario = mongoose.model("Usuario", UsuarioSchema)
-
-
-//configuração dos roteamentos
-//cadastro usuario
-app.post("/cadastroUsuario", async(req, res)=>{
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const endereco = req.body.endereco;
-    const numero = req.body.numero;
-    const cep = req.body.cep;
-    const nascimento = req.body.nascimento;
-
-    const usuario =  new Usuario({
-        nome : nome,
-        email : email,
-        endereco : endereco,
-        numero : numero,
-        cep : cep,
-        nascimento : nascimento
-    })
-    try{
-        const newUsuario = await usuario.save();
-        res.json({error : null, msg : "cadastro ok", UsuarioId : newUsuario._id})
-    } catch(error){}
+  nome: { type: String },
+  email: { type: String, required: true },
+  endereco: { type: String },
+  numero: { type: Number },
+  cep: { type: String, required: true },
+  nascimento: { type: Date, required: true }
 });
 
-app.get("/",async(req,res)=>{
-    _dirname("/index.html");
-})
+const Usuario = mongoose.model("Usuario", UsuarioSchema);
+
+//configuração dos roteamendos
+//cadastrousuario
+app.post("/cadastrousuario", async (req, res) => {
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const endereco = req.body.endereco;
+  const numero = req.body.numero;
+  const cep = req.body.cep;
+  const nascimento = req.body.nascimento;
+
+  const usuario = new Usuario({
+    nome: nome,
+    email: email,
+    endereco: endereco,
+    numero: numero,
+    cep: cep,
+    nascimento: nascimento
+  });
+
+  const emailExiste = await Usuario.findOne({email : email});
+  const cepTamanho = cep.length
+
+  if(nome == null || email == null || endereco == null || numero == null || cep == null || nascimento == null){
+    return res.status(400).json({error : "preencha todos os campos"})
+  }
+  
+  else if(emailExiste){
+    return res.status(400).json({error : "o email existe no nosso banco de dados"})
+  }
+
+  else if(cepTamanho > 11 || cepTamanho < 11){
+    return res.status(400).json({error : "tamanho do cep esta errado"})  
+  }
+
+  try {
+    const newUsuario = await usuario.save();
+    res.json({ error: null, msg: "Cadastro ok", UsuarioId: newUsuario._id });
+    console.log("cadastro feito do usuario:" + newUsuario._id);
+  } catch (error) {}
+});
+
+//rota de get de formulario
+app.get("/cadastrousuario", async(req, res)=>{
+  res.sendFile(__dirname + "/cadastrousuario.html")
+});
+
+app.get("/", async(req, res)=>{
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+});
